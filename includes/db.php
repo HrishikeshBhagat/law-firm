dd138326f7ae4abe9c2a5910fbe81d3065d5f4ea
hrishikesh <hrishibhagat08@gmail.com>
2025-06-25 22:47:45 +0530

    session bugs fixed while submission

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,9 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "law_firm");
 if ($conn->connect_error) die("DB Error: " . $conn->connect_error);
-session_start();
+
+// To Prevent session_start() warning if session already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
